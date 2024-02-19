114637d98ffc56464de6b2f147a4d074c6149adf
genis <comp.genis@gmail.com>
2024-02-19 13:44:31 -0400

    Agregar validacion de ID de producto al añadirlo al carrito, eliminar archivos de persistencia de memoria

--- Primera preentrega/src/routes/carts.router.js
+++ Primera preentrega/src/routes/carts.router.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import CartManager from "../CartManager.js";
+import ProductManager from "../ProductManager.js";
+
 
 const router = Router();
 // crear base de datos
 const cartManager = new CartManager
-
+const productManager = new ProductManager
 router.post('/', (req,res)=> {
     cartManager.addCart()
     res.send({ status: "Success", msg: 'Carrito creado' })
@@ -24,8 +26,15 @@ router.get('/:cid', (req, res) => {
 router.post('/:cid/product/:pid', (req, res)=> {
     const newProductId = req.params.pid
     const cartId = req.params.cid
-    cartManager.addProdinCart(cartId, newProductId);
-    res.send({ status: "Success", msg: 'El producto fue añadido al carrito' })
+    const product = productManager.getProductById(parseInt(newProductId));
+    if (!product) {
+        res.status(404).send(`Producto con id "${newProductId}" no encontrado`);
+    }
+    else {
+        cartManager.addProdinCart(cartId, newProductId);
+        res.send({ status: "Success", msg: 'El producto fue añadido al carrito' })
+    }
+   
 })
 
 export default router;
